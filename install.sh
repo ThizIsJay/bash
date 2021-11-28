b486a6b7e307fe817996a6d9c7bc497ba7aee703
Jay <95102557+ThizIsJay@users.noreply.github.com>
2021-11-28 19:34:36 +0100

    Bugfix install.sh

--- install.sh
+++ install.sh
@@ -2,10 +2,16 @@
 echo "Lijst van gebruikers: "
 ls -lAh /home/ | awk '{print $3}'
 read -p 'Selecteer een gebruiker: ' myUser 2>&1
-#git clone https://github.com/thizisjay/bash ~/.config/bash
 
-
+mkdir -p /home/$myUser/.config/bash/
+wget https://raw.githubusercontent.com/ThizIsJay/bash/master/bashrc -O /home/$myUser/.config/bash/bashrc
+wget https://raw.githubusercontent.com/ThizIsJay/bash/master/functions.sh -O /home/$myUser/.config/bash/functions.sh
+wget https://raw.githubusercontent.com/ThizIsJay/bash/master/starship.toml -O /home/$myUser/.config/bash/starship.toml
+sudo chown $myUser:users /home/$myUser/.config/bash
+sudo chown $myUser:users /home/$myUser/.config/bash/bashrc
+sudo chown $myUser:users /home/$myUser/.config/bash/functions.sh
+sudo chown $myUser:users /home/$myUser/.config/bash/starship.toml
 sudo cat > /etc/bash.bashrc <<EOF
 if [ -s /home/$myUser/.config/bash/bashrc ]; then
-	. '/home/$myUser/.config/bash/bashrc'; fi
+        . '/home/$myUser/.config/bash/bashrc'; fi
 EOF
